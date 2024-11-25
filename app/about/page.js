d012f9d2c2996d9310a143b682e5b6a44588317c
sahil khatri <sahilkeshav1997@gmail.com>
2024-11-25 09:02:35 +0530

    link component

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const About = () => {
   return (
     <div>
       <h1 className='text-7xl'>About Page</h1>
+      <Link href='/' className='text-2xl'>
+        home page
+      </Link>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <h1 className='text-7xl'>Home Page</h1>
+      <Link href='/about' className='text-2xl'>
+        about page
+      </Link>
     </div>
   )
 }
